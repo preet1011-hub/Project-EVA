9a11b8e2a20809ceb53457052b2f4ce08d4b03eb
preet1011-hub <83107043+preet1011-hub@users.noreply.github.com>
2023-06-30 21:35:11 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+# todo: Add your api key here
+apikey = "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"
+news_api_key = "https://e6d49e4c539c42b1a22c45184863b253.weavy.io"
+weather_api_key = ""
