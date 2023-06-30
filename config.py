527f8807591c9c46d0ee037c6db538ae61d1567b
preet1011-hub <83107043+preet1011-hub@users.noreply.github.com>
2023-06-30 21:36:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 # todo: Add your api key here
-apikey = "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"
-news_api_key = "https://e6d49e4c539c42b1a22c45184863b253.weavy.io"
-weather_api_key = ""
+#Get it from OPENAI Website for free
+apikey = "ENTER YOUR APIKEY"
